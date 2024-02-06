db7ab13059ad31b0af9d074c48e418c11d8d3479
MatNiem <niemiec512@gmail.com>
2024-02-06 21:50:14 +0100

    gwint part completed -> added gwint cards pictures to gwint card selector with tabs for each deck

--- cards.py
+++ cards.py
@@ -1,59 +1,117 @@
 import flet as ft
-from flet import Page, DataTable, DataColumn, DataRow, DataCell, ElevatedButton, Text, TextField, View, Row, AppBar, Image
+from flet import (Page, Tabs, Tab, Container, Stack, ElevatedButton,
+                  Text, TextField, View, Row, AppBar, Image, UserControl, GridView)
 
 import csv
 
 
+class CardsTab(UserControl):
+    def __init__(self, deck: str, page: Page) -> None:
+        super().__init__()
+        self.deck = deck
+        self.page = page
+        self.grid = self.gen_row()
+
+    def button_clicked(self, e: ft.ControlEvent) -> None:
+        print(e.control.data)
+        self.page.set_clipboard(e.control.data)
+
+    def gen_row(self):
+        grid = GridView(max_extent=self.page.width/5, child_aspect_ratio=0.5)
+
+        data = []
+        with open("gwent_cards.csv", "r") as f:
+            f.readline()
+            file_content = f.readlines()
+
+        data = [line.strip().split(",") for line in file_content]
+
+        for d in data:
+            deck = d[0]
+            name = d[1]
+            picture_url = d[2]
+            card_id = d[3]
+            # print(name)
+            if deck == self.deck:
+                grid.controls.append(
+                    Container(
+                        content=
+                        # Text(name),
+                        Image(src=picture_url, border_radius=ft.border_radius.all(10)),
+                        # ink=True,
+                        bgcolor='',
+                        margin=10,
+                        padding=10,
+                        height=200,
+                        width=100,
+                        alignment=ft.alignment.center,
+                        on_click=self.button_clicked,
+                        data=f"additem('{card_id}')"
+                    )
+
+                    # ElevatedButton(text=name,
+                    #                data=f"additem('{card_id}')",
+                    #                on_click=self.button_clicked,
+                    #                )
+
+
+                    # ElevatedButton(text=name,
+                    #                height=300,
+                    #                width=150,
+                    #                content=Image(src=picture_url,
+                    #                              border_radius=ft.border_radius.all(10),
+                    #                              ),
+                    #                data=f"additem('{card_id}')",
+                    #                on_click=self.button_clicked)
+                )
+        return grid
+
+    def build(self) -> GridView:
+        return self.grid
+
+
 def gen_cards_view(page: Page):
-    with open("gwent_cards.csv", "r") as f:
-        f.readline()
-        dane = f.readlines()
-
-    # Podziel dane na wiersze
-
-    wiersze = [wiersz.strip().split(",") for wiersz in dane]
-
-    rows = []
-
-    def button_clicked(e: ft.ControlEvent):
-        command = f"additem('{e.control.data}')"
-        print(command)
-        page.set_clipboard(command)
-
-    def gen_table(wierszyk):
-        for wiersz in wierszyk:
-            deck = wiersz[0]
-            name = wiersz[1]
-            picture = wiersz[2]
-            card_id = wiersz[3]
-            rows.append(
-                DataRow(cells=[
-                    DataCell(Text(deck)),
-                    DataCell(ElevatedButton(name, on_click=button_clicked, data=card_id)),
-                    # DataCell(Image(src=picture))
-                    DataCell(Text(card_id))
-                ]),
-            )
-
-    gen_table(wiersze)
 
     view = View(
         route='/items',
         controls=[
-            AppBar(title=Text("Items"), bgcolor='blue'),
+            AppBar(title=Text("Cards"), bgcolor='blue'),
 
-            DataTable(
-                columns=[
-                    DataColumn(Text("Deck")),
-                    DataColumn(Text("Card")),
-                    DataColumn(Text("id"))
+            Tabs(
+                selected_index=1,
+                animation_duration=300,
+                tabs=[
+                    Tab(
+                        text="Northern Realms",
+                        content=CardsTab(deck="Northern Realms", page=page)
+                    ),
+                    Tab(
+                        text="Nilfgaard",
+                        content=CardsTab(deck="Nilfgaard", page=page)
+                    ),
+                    Tab(
+                        text="Monsters",
+                        content=CardsTab(deck="Monsters", page=page)
+                    ),
+                    Tab(
+                        text="Scoia'tael",
+                        content=CardsTab(deck="Scoia'tael", page=page)
+                    ),
+                    Tab(
+                        text="Neutral",
+                        content=CardsTab(deck="Neutral", page=page)
+                    ),
+                    Tab(
+                        text="Skellige",
+                        content=CardsTab(deck="Skellige", page=page),
+                    )
                 ],
-                rows=rows,
+                width=page.width,
+                height=page.height - 40,
             )
         ],
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
     )
 
-    view.scroll = True
-
+    view.scroll = False
     return view
